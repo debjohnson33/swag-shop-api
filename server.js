399100225d974ddf7c90b18ae8927e44b6654df7
debjohnson <debjohnson_11@yahoo.com>
2018-05-29 16:11:46 -0500

    Added app post for product to server

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ var db = mongoose.connect('mongodb://localhost/swag-shop');
 var Product = require('./model/product');
 var WishList = require('./model/wishlist');
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/product', function(req, res) {
+	var product = new Product();
+	product.title = req.body.title;
+	product.price = req.body.price;
+	product.save(function(err, savedProduct) {
+		if (err) {
+			res.status(500).send({error: "Could not save product"});
+		} else {
+			res.send(savedProduct);
+		}
+	});
+});
 
 app.listen(3000, function() {
 	console.log("Swag Shop API running on port 3000...");
