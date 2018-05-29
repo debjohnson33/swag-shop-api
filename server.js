424c28b8016857365c0f12203970849375976d87
debjohnson <debjohnson_11@yahoo.com>
2018-05-29 17:01:53 -0500

    Edited wishlist get response to populate product list

--- server.js
+++ server.js
@@ -33,6 +33,46 @@ app.get('/product', function(req, res) {
 	});
 });
 
+app.get('/wishlist', function(req, res) {
+	WishList.find({}).populate({path:'products', model:'Product'})
+		.exec(function(err, wishLists) {
+			if (err) {
+				res.status(500).send({error: "Could not fetch wishlists"});
+			} else {
+				res.status(200).send(wishLists);
+			}
+		});
+});
+
+app.put('/wishlist/product/add', function(req, res) {
+	Product.findOne({_id: req.body.productId}, function(err, product) {
+		if (err) {
+			res.status(500).send({error: "Could not add item to wishlist"});
+		} else {
+			WishList.update({_id: req.body.wishListId}, {$addToSet: 
+				{products: product._id}}, function(err, wishList) {
+					if (err) {
+						res.status(500).send({error: "Could not add item to wishlist"});
+					} else {
+						res.send("Successfully added to wishlist");
+					}
+			});
+		}
+	});
+});
+
+app.post('/wishlist', function(req, res) {
+	var wishList = new WishList();
+	wishList.title = req.body.title;
+	wishList.save(function(err, newWishList) {
+		if (err) {
+			res.status(500).send({error: "Could not create wishlist"});
+		} else {
+			res.send(newWishList);
+		}
+	});
+});
+	
 app.listen(3000, function() {
 	console.log("Swag Shop API running on port 3000...");
 });
